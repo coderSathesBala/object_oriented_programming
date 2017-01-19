f50d385d27e3463f18820dce024a35dc66834b25
Sathes Balasingam <sathes352@gmail.com>
2017-01-18 20:07:31 -0500

    commit

---
+++ objectexercise1.rb
@@ -0,0 +1,23 @@
+  class BankAccount
+  attr_accessor :balance, :interest_rate
+
+  def initialize(balance, interest_rate)
+    @balance = balance
+    @interest_rate = interest_rate
+  end
+
+def increase(deposit_amount)
+  @balance += deposit_amount
+  puts "ADDING #{@balance} + #{deposit_amount}"
+end
+
+def decrease(deposit_amount)
+  @balance -= deposit_amount
+  puts "SUBTRACT #{@balance} - #{deposit_amount}"
+end
+
+  def interest
+    @balance = @balance + (@balance*(@interest_rate))
+    puts "MULTIPLICATION #{balance} * #{interest_rate}"
+  end
+  end
